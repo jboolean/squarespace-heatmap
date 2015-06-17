a7440116385bef7b177dabcd18618696dbe981d5
abourai@squarespace.com <abourai@squarespace.com>
2015-06-16 23:20:08 -0400

    created flask server, adding Mongo

---
+++ flask-server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from pymongo import MongoClient
+import gridfs
+
+import json
+
+app = Flask(__name__)
+
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+db = client['track-database']
+clicks = db['clicks']
+hovers = db['hovers']
+
+@app.route('/', methods = ['GET','POST'])
+def signal():
+    if request.method == 'POST':
+        print request.form
+        for value in request.form:
+            clicks = db.clicks
+            print clicks
+            print db.hovers
+            post = {'clicks': request.form[value]}
+            print post
+            #clicks.insert_one(post)
+            #db.clicks.insert_one()
+        print clicks.find_one()
+    else:
+        print request.form
+
+
+
+if __name__ == '__main__':
+    app.run()

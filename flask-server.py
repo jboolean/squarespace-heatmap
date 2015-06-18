34395b35a11f8f1dc9eff976d04de606e8c01bd5
abourai@squarespace.com <abourai@squarespace.com>
2015-06-18 00:46:06 -0400

    fixed GET and POST methods, now capable of storing to Mongo and querying from Mongo

--- flask-server.py
+++ flask-server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from pymongo import MongoClient
 import gridfs
+import pymongo
 
 import json
 
@@ -14,21 +15,45 @@ hovers = db['hovers']
 
 @app.route('/', methods = ['GET','POST'])
 def signal():
+
     if request.method == 'POST':
-        print request.form
-        for value in request.form:
-            clicks = db.clicks
-            print clicks
-            print db.hovers
-            post = {'clicks': request.form[value]}
-            print post
-            print db.clicks.insert_one(post).inserted_id
-            print "continuation"
+
+        print "POST"
+        mongo_save(request)
     else:
-        print db.clicks.find_one()
-        print request.form
+        print "GET"
+        mongo_get(request)
+    return "done"
 
 
+def mongo_save(request):
+    for value in request.form:
+
+        data = request.form[value].encode('ascii','ignore')
+        clickCount, hoverCount = eval(data)
+
+        sendClicks = {"name" : value, "value": clickCount}
+        sendHovers = {"name" : value, "value": hoverCount}
+
+        db.clicks.save(sendClicks)
+        db.hovers.save(sendHovers)
+
+    print "saved"
+    return "saved"
+
+def mongo_get(request):
+    print request.args
+    for value in request.args:
+        print value
+        print request.args[value]
+        data = request.args[value].encode('ascii','ignore')
+        if "clicks" in data:
+            print value, list(db.clicks.find({"name": value}))
+        if "hovers" in data:
+            print value, list(db.hovers.find({"name": value}))
+        #print mongo.find(value)
+
+    return "none"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
